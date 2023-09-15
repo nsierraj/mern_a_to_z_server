84cbadffeb79dd3f7a88b58cf66e4c6cfa25b87f
Amado Sierra <sierra.amado@gmail.com>
2023-09-14 21:09:52 -0400

    galaxies

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world and universe!'));
+app.get('/', (req, res) => res.send('Hello world, galaxies, and universe!'));
 
 app.use('/api/books', books);
 
