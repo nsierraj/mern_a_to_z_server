8c1629a93d634bf0bc3cc263648fbcff5af35dd3
Amado Sierra <sierra.amado@gmail.com>
2023-09-29 08:30:35 -0400

    earth

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world, galaxies, and universe ✨✨!'));
+app.get('/', (req, res) => res.send('Hello world 🌎, galaxies ✨, and universe 🪐!'));
 
 app.use('/api/books', books);
 
--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+//const db = config.get('mongoURI');
+require('dotenv').config()
+const db = process.env.DB_CONN;
 
 const connectDB = async () => {
   try {
